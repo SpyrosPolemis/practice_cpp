2c776ac140d07954bdaadcd71a98be5dfdd293bf
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-10-03 22:39:17 +1000

    Initial commit

---
+++ Vectors/whale.cpp
@@ -0,0 +1,15 @@
+/*
+ * Written by, Spyros Polemis, 03/10/2024
+ *
+ * Takes a string and translates it into its "whale talk" equivalent:
+ * Only vowels, u's and e's twice as long
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main () {
+
+}
