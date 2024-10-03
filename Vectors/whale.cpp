9f8d0b03e9c09245961f436be4303c156e47dba8
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-10-03 23:18:47 +1000

    Feature: Translate human-talk into whale-talk

--- Vectors/whale.cpp
+++ Vectors/whale.cpp
@@ -12,9 +12,31 @@
 
 int main () {
 
+ //Get user input
  std::string human_talk;
  std::cout << "Enter text: ";
  std::cin >> human_talk;
 
+ std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+ std::vector<char> whale_talk;
 
+ // Translate human talk into whale talk
+ for (char i : human_talk) {
+  for (char j : vowels) {
+   if (j == i) {
+    whale_talk.push_back(i);
+   }
+  }
+  // Double e's and i's
+  if (i == 'e') {
+   whale_talk.push_back('e');
+  } else if (i == 'i') {
+   whale_talk.push_back('i');
+  }
+ }
+
+ // Print whale talk
+ for (char i : whale_talk) {
+  std::cout << i;
+ }
 }
