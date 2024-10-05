d381ab3f1e347c09abfad5b1d70d42137658c04f
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-10-05 20:59:52 +1000

    Feature: Read file up to and get line at random number generated

--- magic8/magic8.cpp
+++ magic8/magic8.cpp
@@ -7,8 +7,22 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 int main() {
 	srand((unsigned) time(0));
 	int random_num = rand() % 21;
+
+	std::string bin = ""; // Create a bin to put all lines before random
+	std::string fortune = ""; // Create var for actual fortune
+
+	std::ifstream MyFile("magic8.txt"); //open txt file for reading as MyFile
+	if (MyFile.is_open()) { // Check file is open, why?
+		// for loop puts cursor at right place
+		for (int i = 0; i < random_num - 1; i++) {
+			getline(MyFile, bin);
+			std::cout << fortune;
+		}
+		getline(MyFile, fortune);
+	}
 }
