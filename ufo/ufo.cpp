ef646fc660d0baf387ae246453683f4e0192d07a
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-10-05 21:17:43 +1000

    Initial commit

---
+++ ufo/ufo.cpp
@@ -0,0 +1,17 @@
+/*
+ * Written by, Spyros Polemis, 05/10/2024
+ *
+ * Hangman with a less? grim premise
+ *
+ * Written to practice using functions
+ */
+
+
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+
+  // TODO: Write main
+
+}
---
+++ ufo/ufo_functions.hpp
@@ -0,0 +1,6 @@
+#pragma once // IDE recommendation - prevents header file from being included multiple times
+#include <vector>
+
+void display_misses(int misses);
+
+// TODO: Declare functions
